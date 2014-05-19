b0ba90c1e88da189e56d58d2ade6434c8ad1eb07
Laurent Pouilloux <laurent.pouilloux@inria.fr>
2014-05-19 23:31:56 +0200

    ajout d'une redirection si pas de fichier de config.php

--- index.php
+++ index.php
@@ -1,35 +1,32 @@
 <?php
-// Configuration
+$config_file = 'app/config.php';
+if (!file_exists($config_file)) {
+	echo 'Pas de fichier de config trouv&eacute;, redirection vers le script d\'installation';
+	require_once('app/includes/setup/redirect.php');
+} else {
+	// Configuration
+	require_once('app/config.php');
+	session_start();
+	// Fonctions du site
+	require_once('app/includes/common/fonctions.php');
 
-require_once('app/includes/config.php');
-session_start();
-// Fonctions du site
-require_once('app/includes/common/fonctions.php');
+	// Connexion à la base de données
+	require_once('app/includes/common/db_connect.php');
 
-// Connexion à la base de données
-require_once('app/includes/common/db_connect.php');
+	// Test de connexion d'un utilisateur
+	require_once('app/includes/common/user_connect.php');
 
-// Test de connexion d'un utilisateur
-require_once('app/includes/common/user_connect.php');
+	// Exécution des requètes
+	require_once('app/includes/common/execution_requetes.php');
 
-// Exécution des requètes
-require_once('app/includes/common/execution_requetes.php');
+	// Récupération du nom de la page
+	require_once('app/includes/common/data_page.php');
 
-// Récupération du nom de la page
-require_once('app/includes/common/data_page.php');
+	// Rendu de la page
+	require_once('app/includes/common/render_page.php');
 
+	// Déconnexion de la base de données
+	require_once('app/includes/common/db_close.php');
+}
 
-// Rendu de la page
-require_once('app/includes/common/render_page.php');
-
-// Déconnexion de la base de données
-require_once('app/includes/common/db_close.php');
-
-
-/*
-echo '<pre>';
-print_r($_POST);
-print_r($_SESSION);
-echo '</pre>';
-*/
 ?>
