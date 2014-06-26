cc4ce39673ceff7fd82b6f3081af725f4dffd1e1
Laurent Pouilloux <laurent.pouilloux@inria.fr>
2014-06-26 22:36:58 +0200

    fix une typo dans la vue du groupe

--- app/includes/concours/par_groupe.php
+++ app/includes/concours/par_groupe.php
@@ -28,10 +28,10 @@ while ($d_user=mysqli_fetch_array($r_user)) {
 	$cestmoi=(isset($_SESSION['id_user']) and $_SESSION['id_user']==$d_user['id_user'])?
 			'<strong id="cestmoi">'.htmlentities($d_user['login'],ENT_QUOTES,'UTF-8').'</strong>':
 			htmlentities($d_user['login'],ENT_QUOTES,'UTF-8');
+
 	$html.='<li>'.$puce.' '.
 		$d_user['login'].' '.$d_user['points'].' points';
-
-	'</li>';
+	$html.='</li>';
 }
 $html.='</ul>';
 ?>
