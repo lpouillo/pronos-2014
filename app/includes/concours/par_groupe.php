98324ec830687947f3e5882551dbd2086e0f9599
Laurent Pouilloux <laurent.pouilloux@inria.fr>
2014-07-04 15:05:55 +0200

    nettoyage

--- app/includes/concours/par_groupe.php
+++ app/includes/concours/par_groupe.php
@@ -27,14 +27,14 @@ while ($d_user=mysqli_fetch_array($r_user)) {
 		$close_h2=false;
 	}
 	$puce=get_puce($d_user['classement']);
-	$cestmoi=(isset($_SESSION['id_user']) and $_SESSION['id_user']==$d_user['id_user'])?
+	$cestmoi=(isset($_SESSION['id_user']) and $_SESSION['id_user']==$d_user['owner'])?
 			'<strong id="cestmoi">'.htmlentities($d_user['login'],ENT_QUOTES,'UTF-8').'</strong>':
 			htmlentities($d_user['login'],ENT_QUOTES,'UTF-8');
 	if ($is_owner) {
-		$html.=($d_groupe['actif'])?'':'<a href="index.php?page=mon_espace&section=mes_groupes' .
-			'&action=activer&id='.$_GET['id'].'_'.$d_groupe['id_user'].'">' .
-			'<img src="public/images/icons/user_add.png" alt="activer" title="ajouter l\'utilisateur à ce groupe"/>' .
-			'</a>';
+//		$html.=($d_groupe['actif'])?'':'<a href="index.php?page=mon_espace&section=mes_groupes' .
+//			'&action=activer&id='.$_GET['id'].'_'.$d_groupe['id_user'].'">' .
+//			'<img src="public/images/icons/user_add.png" alt="activer" title="ajouter l\'utilisateur à ce groupe"/>' .
+//			'</a>';
 	}
 	$html.='<li>'.$puce.' '.
 		$d_user['login'].' '.$d_user['points'].' points';
