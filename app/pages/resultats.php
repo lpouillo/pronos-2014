f13aa0f1fbce3b50aca71abd2eaf377e765771d4
Laurent Pouilloux <laurent.pouilloux@ec-lyon.fr>
2016-06-04 00:13:19 +0200

    ajout d'un espace entre poule et tableau

--- app/pages/resultats.php
+++ app/pages/resultats.php
@@ -3,18 +3,17 @@ $html = '';
 if (empty($_GET['section'])) {
 	if (time()<$timestamp_poules_fin) {
 		require_once('app/includes/resultats/poules.php');
+		$html.='<br/>';
 		require_once('app/includes/resultats/tableau.php');
 	} else {
 		require_once('app/includes/resultats/tableau.php');
+		$html.='<br/>';
 		require_once('app/includes/resultats/poules.php');
 	}
 } else {
 	switch($_GET['section']) {
 		case 'match':
 			include('app/includes/resultats/match.php');
-		break;
-		case 'relief':
-			include('app/includes/concours/relief.php');
 		break;
 	}
 }
